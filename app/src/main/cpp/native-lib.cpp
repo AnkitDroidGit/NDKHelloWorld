6e4c60f052553e9111cfdf454290530c5c11a825
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-10-29 14:41:59 +0800

    working on it

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,3 +1,14 @@
 #include <jni.h>
 #include <string>
 
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_cogitator_ndkhelloworld_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+
+    return env->NewStringUTF(hello.c_str());
+}
--- app/src/main/java/com/cogitator/ndkhelloworld/MainActivity.kt
+++ app/src/main/java/com/cogitator/ndkhelloworld/MainActivity.kt
@@ -1,9 +1,37 @@
 package com.cogitator.ndkhelloworld
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * @author Ankit Kumar on 29/10/2018
  */
-class MainActivity:AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI()
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Example of a call to a native method
+        stringFromJNI()
+
+        text.text = stringFromJNI()
+    }
+
 }
